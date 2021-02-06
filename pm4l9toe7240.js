81d1d7950e0794104205cd4cc77768463fc3e044
DelfinProtector <78093684+DelfinBot@users.noreply.github.com>
2021-02-07 01:56:30 +0300

    Auto-Protect

--- pm4l9toe7240.js
+++
@@ -1,5 +0,0 @@
-BILDIRILEN TOKEN: Nzk3NDE0NTMzOTY1OTM4NzA4.X_mIKA.31CSF0sgzVn5sYC5N4v6UQX62Vk
-HIZ: 0:00:00
-
-Delfin: Eğer bu dosyayı görüyorsanız tokeniniz bildirilmiş demektir. Bu dosya token yenilendikten sonra silinmiştir. Lütfen endişe etmeyiniz. Bir daha böyle bir olay yaşamamak için lütfen tokenlerinizi halka açık alanlarda paylaşmayınız.
--abs ~(˘▾˘~)
