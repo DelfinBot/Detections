4fac3210d110a9fa6dc5b5d44fbf46d7c099120f
DelfinProtector <78093684+DelfinBot@users.noreply.github.com>
2021-02-07 01:56:26 +0300

    Auto-Protect

---
+++ vidgo37bxabec7.lua
@@ -0,0 +1,5 @@
+BILDIRILEN TOKEN: ODA0MDA4MTQ1NTQ2Mzc5Mjc1.YBGE7w.oH4fzdQtdxEkyn1tKf_Y6kF6oGk
+HIZ: 0:00:00.002999
+
+Delfin: Eğer bu dosyayı görüyorsanız tokeniniz bildirilmiş demektir. Bu dosya token yenilendikten sonra silinmiştir. Lütfen endişe etmeyiniz. Bir daha böyle bir olay yaşamamak için lütfen tokenlerinizi halka açık alanlarda paylaşmayınız.
+-abs ~(˘▾˘~)
